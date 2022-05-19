4c416c5c25d397ba75c1a0324f30173b3f649fe4
Afdhan <77567725+Afdhan@users.noreply.github.com>
2022-05-20 00:04:01 +0700

    Update ceklim.sh

--- ceklim.sh
+++ ceklim.sh
@@ -4,6 +4,15 @@ GREEN='\e[0;32m'
 BLUE='\e[0;34m'
 NC='\e[0m'
 MYIP=$(wget -qO- ipinfo.io/ip);
+echo "Checking VPS"
+IZIN=$( curl https://worldssh.tech/api/sc/akses.php | grep $MYIP )
+if [ $MYIP = $IZIN ]; then
+echo -e "${NC}${GREEN}Permission Accepted...${NC}"
+else
+echo -e "${NC}${RED}Permission Denied!${NC}";
+exit 0
+fi 
+
 
 clear
 echo " "
