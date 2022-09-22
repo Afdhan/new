bb8cddbcf9123ddd05f1a780df301e390154e045
Afdhan <77567725+Afdhan@users.noreply.github.com>
2022-09-22 21:28:05 +0700

    Update menu.sh

--- menu.sh
+++ menu.sh
@@ -20,7 +20,7 @@ LIGHT='\033[0;37m'
 
 MYIP=$(wget -qO- ipinfo.io/ip);
 echo "Checking VPS"
-IZIN=$( curl https://worldssh.tech/api/sc/akses.php | grep $MYIP )
+IZIN=$( curl https://worldssh.com/api/sc/akses.php | grep $MYIP )
 if [ $MYIP = $IZIN ]; then
 echo -e "${NC}${GREEN}Permission Accepted...${NC}"
 else
